3cb2b20a960acbabe1d1cf9747bdd3e5c01e6b71
saidshah <saidmshah93@gmail.com>
2023-05-10 12:08:52 -0500

    max char

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,24 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let maxCount = 0;   //to store the max number
+    let maxChar = '';   //to stor the max character
+    const chars = {};   //to put chars in an object to compare
+    for(let char of str){
+        if(!chars[char]){       //if the char does not exist, throw a 1
+            chars[char] = 1;
+        }else{
+            chars[char]++;      //if the char already exist increment it
+        }
+        if(chars[char] > maxCount){//if the char number in object is greater than the maxCount var
+            maxCount = chars[char];//give the var the value of the incremented number
+            maxChar = char;         //the character with highest val is the maxChar
+        }
+    }
+    return maxChar;
+}
+console.log(maxChar("apple"));
+console.log(maxChar("tthe highest mount"));
 
-module.exports = maxChar;
+// module.exports = maxChar;
