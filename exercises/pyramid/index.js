b5377a879c3cb9e403310aedeb24173f4f15c3d0
saidshah <saidmshah93@gmail.com>
2023-05-18 01:25:07 -0500

    pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,21 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+    const midPoint = Math.floor((2 * n - 1)/2);
+    for (let row = 0; row < n; row++){
+        let level = '';
+        for(let column = 0; column < 2 * n - 1; column++){
+            if(midPoint - row <= column && midPoint + row >= column){
+                level += '#';
+            }else{
+                level += ' ';
+            }
+        }
+        console.log(level);
+    }
+}
+
+pyramid(5);
 
 module.exports = pyramid;
